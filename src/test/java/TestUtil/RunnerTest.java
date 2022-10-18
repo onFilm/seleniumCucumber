2ee23aaaa5bf3621e5c85197a8bdd0c117c6b4fd
Prajwal Acharya <mail2prajwal12@gmail.com>
2022-10-18 20:27:58 +0530

    update reports to reports/index.html

--- src/test/java/TestUtil/RunnerTest.java
+++ src/test/java/TestUtil/RunnerTest.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = { "pretty","html:target/cucumber-reports.html" }
+@CucumberOptions(plugin = { "pretty","html:reports/index.html" }
         ,features = {"src/test/resources"}
         ,glue = {"commonUtils","stepDefination"}
         ,monochrome = true
--- src/test/java/TestUtil/TestNGRunnerTest.java
+++ src/test/java/TestUtil/TestNGRunnerTest.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
-@CucumberOptions(plugin = { "pretty","html:target/cucumber-reports.html" }
+@CucumberOptions(plugin = { "pretty","html:reports/index.html" }
         ,features = {"src/test/resources"}
         ,glue = {"commonUtils","stepDefination"},
         publish = true
